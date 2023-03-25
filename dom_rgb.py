2b912a5963a2c9e5d741cfc6e510d48b08ea294e
Sam Tholath Robin <92199373+samtholathrobin@users.noreply.github.com>
2023-03-25 11:24:31 +0530

    Update dom_rgb.py

--- dom_rgb.py
+++ dom_rgb.py
@@ -3,12 +3,8 @@ import numpy as np
 import scipy
 from scipy import misc 
 from scipy import cluster
-import binascii as bsc
-import time
-# from fastapi import FastAPI
-st=time.time()
-# app=FastAPI()
-img = Image.open("C:/Users/samth/Documents/Python Scripts/img_to_rgb/rocket.png")
+
+img = Image.open("(input source)")
 l,b=img.size
 img=img.resize((int(l/4), int(b/4)))
 ar=np.asarray(img)
@@ -23,8 +19,3 @@ counts, bins = np.histogram(vecs, len(codes))
 idx_max=np.argmax(counts)
 peak=codes[idx_max]
 print('r: %f\tg: %f\tb: %f' %(peak[0],peak[1],peak[2]))
-et=time.time()
-print("time: ",et-st,"ms")
-# @app.get("/")
-# def read_root(): 
-#     return {"message":(peak[0],peak[1],peak[2])}
